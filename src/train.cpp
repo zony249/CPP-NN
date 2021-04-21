91acbbe524518eedbd9443aac083e2066ea5c59b
Zony Yu <zony249@gmail.com>
2021-04-20 23:39:42 -0600

    hyperparameter tuning

--- src/train.cpp
+++ src/train.cpp
@@ -16,16 +16,17 @@ int main(int argc, char* argv[])
     string train_filename = "MNIST-data-set/mnist_train.csv";
     string test_filename = "MNIST-data-set/mnist_test.csv";
     vector <pair<Matrix<float>, Matrix<float> > > trainset = read_MNIST_csv<float> (train_filename);
-    vector <pair<Matrix<float>, Matrix<float> > > testset = read_MNIST_csv<float> (test_filename);
+    vector <pair<Matrix<float>, Matrix<float> > > cvset = read_MNIST_csv<float> (test_filename);
     cout << "Num of training data: " << trainset.size() << endl;
-    cout << "Num of testing data: " << testset.size() << endl << endl;
+    cout << "Num of cross val data: " << cvset.size() << endl << endl;
     //cout << trainset[8010].second << endl;
 
-    Model<float> model(1e-3, 0.1);
+    Model<float> model(8e-4, 0.3);
     model.add_layer(Layer<float>(784, "linear"));
-    model.add_layer(Layer<float>(256, "relu"));
-    model.add_layer(Layer<float>(256, "relu"));
-    model.add_layer(Layer<float>(256, "relu"));
+    //model.add_layer(Layer<float>(64, "relu"));
+    model.add_layer(Layer<float>(128, "relu"));
+    model.add_layer(Layer<float>(128, "relu"));
+    //model.add_layer(Layer<float>(64, "relu"));
     model.add_layer(Layer<float>(10, "sigmoid"));
     model.connect();
 
@@ -36,46 +37,22 @@ int main(int argc, char* argv[])
     getline(cin, x);
 
     model.load_training_data(&trainset);
+    model.load_cv_data(&cvset);
 
     //Layer<float> L(5, "relu");
     //L.z = Matrix<float>({-2, -1, 0, 1, 2}).tp();
     //cout << L.d_act(L.z) << endl; 
 
-    for (int i = 0; i < 1; i++) model.train(16);
-
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 1; i++)
     {   
-        cout << model.predict(trainset[i].first.tp()).tp() << endl;
-        cout << trainset[i].second << endl << endl << endl;
+        cout << "Epoch " << i+1 << " Starting... " << endl;
+        this_thread::sleep_for(chrono::seconds(2));
+        model.train(16);
+        cout << endl;
+        float cv_accuracy = model.evaluate();
+        cout << "Cross Validation Accuracy: " << cv_accuracy * 100 << "%" << endl;
     }
 
-
-    //Matrix<float> x = train[4].first.tp();
-    //model.forward_prop(x);
-
-    // Model<float> model;
-    // model.add_layer(Layer<float> (4, "linear"));
-    // model.add_layer(Layer<float> (6, "relu"));
-    // model.add_layer(Layer<float> (7, "relu"));
-    // model.add_layer(Layer<float> (8, "relu"));
-    // model.add_layer(Layer<float> (2, "sigmoid"));
-    // model.connect();
-
-    // Matrix<float> x = Matrix<float>({1., 2., 3., 4.}).tp();
-    // Matrix<float> y = Matrix<float>({1, 0}).tp();
-
-    // vector<pair<Matrix<float>, Matrix<float> > > testdat = {{x, y}};
-    // model.load_training_data(&testdat);
-
-    // model.train(16);
-
-    // Matrix<float> C({3, 3}, 3);
-    // Matrix<float> D({3, 3}, 4);
-    // cout << C - D << endl;
-
-
-    // Matrix<float> E({30, 40, 50, 60, 70});
-    // cout << sigmoid(E) << endl;
 
     return 0;
 }
